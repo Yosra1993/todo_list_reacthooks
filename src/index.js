939f3c3ef25e06faa231a1f234a0f66ae8e15946
Devagnos <yosra.m@devagnos.com>
2021-01-17 16:43:10 +0100

    delete env + basename

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.css";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom"; //  add HashRouter + Gh-pages to deploy with react-router 
 import { createBrowserHistory } from "history";
 
 const history = createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL} history={history}> {/* Gh-pages deployment with react-router */}
+    <Router history={history}>
       <App />
     </Router>
   </React.StrictMode>,
